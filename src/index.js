4d0d89c4f871de52a620901cbca34a6d1dbdb3e9
Denis Usanov <denis.usanov@toptal.com>
2015-09-12 17:05:12 +0300

    Fix a typo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import Finn from './lib/finn';
 import Jake from './lib/jake';
 
-const getRandomQoute = (arr) => {
+const getRandomQuote = (arr) => {
   let randomSeason = arr[Math.floor(Math.random() * arr.length)];
   let randomQuote = randomSeason[Math.floor(Math.random() * randomSeason.length)];
   return randomQuote;
 }
 
 export default {
-  getFinnQuote: () => getRandomQoute(Finn),
-  getJakeQuote: () => getRandomQoute(Jake),
-  getQuote: () => getRandomQoute([...Jake, ...Finn])
+  getFinnQuote: () => getRandomQuote(Finn),
+  getJakeQuote: () => getRandomQuote(Jake),
+  getQuote: () => getRandomQuote([...Jake, ...Finn])
 };
